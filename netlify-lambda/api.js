181af275e1e39eb563758b3cfd6eb97128609cba
netcell <anhnt.fami@gmail.com>
2019-04-15 04:29:08 +0700

    include server code in netlify_lambda

--- netlify-lambda/api.js
+++ netlify-lambda/api.js
@@ -1,9 +1,9 @@
 'use strict';
 require('dotenv').config();
 const serverless = require('serverless-http');
-const app = require('../server/app.mjs');
+const app = require('./server/app.mjs');
 
-const booksRoute = require('../server/Routes/Books/index.mjs').booksRoute;
+const booksRoute = require('./server/Routes/Books/index.mjs').booksRoute;
 app.use('/books', booksRoute);
 
 module.exports = app;
